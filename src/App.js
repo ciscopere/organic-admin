de1071998a07983a1780c430fb2fb1a45af634dc
sanuruguayo <santiagopereira4@gmail.com>
2022-06-20 19:29:02 -0300

    material-ui and react

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import SignIn from './SignIn';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Ingenio Organico
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <SignIn/>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
